bce7fa73e3856ac3f17534b491fd832cc567b159
yushinbox@gmail.com <yushinbox@gmail.com>
2021-06-25 19:18:12 +0300

    +

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -9,69 +9,78 @@ import $ from "jquery";
 
 
 // независимые табы-------------------------
-$(document).ready(function () {
-let titleTab=  $(".tabs-title-js");
+$(document).ready(function() {
+    let titleTab = $(".tabs-title-js");
 
-titleTab.on("click", function () {
+    titleTab.on("click", function() {
 
-let dropTab = $(this).siblings(".tabs-drop-js");
-let imgTab = $(this).find(".tabs-img-js");
+        let dropTab = $(this).siblings(".tabs-drop-js");
+        let imgTab = $(this).find(".tabs-img-js");
 
-if ($(this).hasClass("act")) {
-  setTimeout(function () {
-    dropTab.removeClass("act");
-  }, 200);
-  $(this).removeClass("act");
-  dropTab.slideUp(200);
-  imgTab.css("transform", " translateY(-50%) rotate(0deg)");
-}
-
-
-else {
-  $(this).addClass("act");
-  imgTab.css("transform", "translateY(-50%) rotate(180deg)");
-  dropTab.addClass("act").slideDown(200);
-}	
+        if ($(this).hasClass("act")) {
+            setTimeout(function() {
+                dropTab.removeClass("act");
+            }, 200);
+            $(this).removeClass("act");
+            dropTab.slideUp(200);
+            imgTab.css("transform", " translateY(-50%) rotate(0deg)");
+        } else {
+            $(this).addClass("act");
+            imgTab.css("transform", "translateY(-50%) rotate(180deg)");
+            dropTab.addClass("act").slideDown(200);
+        }
 
 
 
-});
+    });
 })
 
 
 
 // // аккордеон --------
-$(document).ready(function (e) {
-  let titleTab = $(".akr-title-js");
+$(document).ready(function(e) {
+    let titleTab = $(".akr-title-js");
 
-  titleTab.on("click", function () {
+    titleTab.on("click", function() {
 
-    let dropTab = $(this).siblings(".akr-drop-js");
-    let imgTab = $(this).find(".akr-item-img-js");
-    let parent = $(this).parent(".akr__item");
+        let dropTab = $(this).siblings(".akr-drop-js");
+        let imgTab = $(this).find(".akr-item-img-js");
+        let parent = $(this).parent(".akr__item");
 
-    if ($(this).hasClass("act")) {
+        if ($(this).hasClass("act")) {
 
 
-      $(this).removeClass("act");
-      dropTab.slideUp(200).removeClass("act");
-      imgTab.css("transform", "translateY(-50%) rotate(0deg)");
-      
-    } else {
+            $(this).removeClass("act");
+            dropTab.slideUp(200).removeClass("act");
+            imgTab.css("transform", "translateY(-50%) rotate(0deg)");
 
-$(this).addClass("act");
-imgTab.css("transform", "translateY(-50%) rotate(180deg)");
-dropTab.addClass("act").slideDown(200);
-    $(".akr__item").not(parent).find(".akr-drop-js").slideUp(200);
-    $(".akr__item").not(parent).find(".akr-title-js").removeClass("act");
-    $(".akr__item")
-      .not(parent)
-      .find(".akr-item-img-js")
-      .css("transform", "translateY(-50%) rotate(0deg)");
-    }
-  });
+        } else {
+
+            $(this).addClass("act");
+            imgTab.css("transform", "translateY(-50%) rotate(180deg)");
+            dropTab.addClass("act").slideDown(200);
+            $(".akr__item").not(parent).find(".akr-drop-js").slideUp(200);
+            $(".akr__item").not(parent).find(".akr-title-js").removeClass("act");
+            $(".akr__item")
+                .not(parent)
+                .find(".akr-item-img-js")
+                .css("transform", "translateY(-50%) rotate(0deg)");
+        }
+    });
 });
 // ----------------------------
 
 
-
+$(document).ready(function(e) {
+    function copytext(el) {
+        var $tmp = $("<input>");
+        $("body").append($tmp);
+        $tmp.val($(el).html()).select();
+        document.execCommand("copy");
+        $tmp.remove();
+    }
+    $(".copy").on("click", function() {
+        let t = $(this).next();
+        copytext(t);
+    });
+});
